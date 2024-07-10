6eb09785416f8925abaa74abfc1561155ede972c
PalindromicBreadLoaf <herbthehaircut@proton.me>
2024-07-10 11:09:05 -0400

    Added controller to manage BagelModel. Currently does nothing of note

---
+++ Bagel/PersistenceController.swift
@@ -0,0 +1,27 @@
+//
+//  PersistenceController.swift
+//  Bagel
+//
+//  Created by Palindromic Bread Loaf on 7/10/24.
+//
+
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+
+    let container: NSPersistentContainer
+
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "BagelModel")
+        if inMemory {
+            container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+    }
+}
+
