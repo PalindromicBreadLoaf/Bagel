58b185599168f9d6f3481a8b39bbf9eadc73e2a8
PalindromicBreadLoaf <herbthehaircut@proton.me>
2024-07-10 10:12:02 -0400

    Initial Commit

---
+++ Bagel/BagelApp.swift
@@ -0,0 +1,17 @@
+//
+//  BagelApp.swift
+//  Bagel
+//
+//  Created by Palindromic Bread Loaf on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BagelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
